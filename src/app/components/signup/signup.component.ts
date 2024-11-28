e0b99b62ece4d70b864e419e561b0c94c8a972ee
João Vitor Schmidt Martins <jaozinholoots2@gmail.com>
2024-11-28 12:26:35 -0300

    create a login screen and register login screen

---
+++ src/app/components/signup/signup.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from '../../helpers/validateform';
+
+@Component({
+  selector: 'app-signup',
+  standalone: false,
+  
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
+})
+export class SignupComponent implements OnInit {
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash"
+  signUpForm!: FormGroup;
+  constructor(private fb: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      email: ['', Validators.required],
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  hideShowPass() {
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
+
+  onSignUp() {
+    if (this.signUpForm.valid) {
+
+    }
+    else {
+      ValidateForm.validateAllFormFields(this.signUpForm)
+    }
+  }
+}
